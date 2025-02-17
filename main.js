5441e2bcb12ae23cdca6ccc9542d5e6a565fbfe0
RukshanRanasinghe2000 <112747422+RukshanRanasinghe2000@users.noreply.github.com>
2025-02-17 13:03:09 +0530

    doc: add doc comments to main code lines and functions

--- main.js
+++ main.js
@@ -1,11 +1,20 @@
+// Create a new Web Worker instance from 'worker.js'
 const worker = new Worker('./worker.js');
 const sunmButton = document.querySelector('#sunmButton');
 const bgButton = document.querySelector('#bgButton');
 
+/**
+ * When clicked, it sends a message to the Web Worker to start the computation.
+ */
 sunmButton.addEventListener('click', () => {
   worker.postMessage("");
 });
 
+
+/**
+ * Event listener for messages received from the Web Worker.
+ * @param {MessageEvent} message - The message event from the Web Worker.
+ */
 worker.onmessage = function(message) {
   alert('Final sum: ' + message.data);
 }
--- worker.js
+++ worker.js
@@ -1,3 +1,9 @@
+/**
+ * Web Worker script for performing a heavy computation task in the background.
+ * The worker listens for messages from the main thread and executes the task.
+ * Once completed, it sends the result back using `postMessage()`.
+ */
+
 onmessage = function(){
     let sum = 0
     for (let i = 0; i < 100000000000; i++) {
